08af890fcf0b30f866e485fc34e44e6e329784eb
DiegoFranciscoG <diegofgz2004@gmail.com>
2025-10-22 12:18:25 -0500

    Primer commit del backend Spring Boot

---
+++ src/main/java/com/ista/springboot/web/app/SistemaMatriculaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ista.springboot.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaMatriculaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaMatriculaBackendApplication.class, args);
+	}
+
+}
